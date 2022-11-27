ff1e7e19d663e4e18061c780cbe321091f18b968
Avishkar Vichare <avie9500@gmail.com>
2022-11-28 04:33:27 +0530

    added cors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const todoRoutes = require('./routes/todoRoutes');
 const taskRoutes = require('./routes/tasksRoutes');
 const userRoutes = require('./routes/userRoutes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
