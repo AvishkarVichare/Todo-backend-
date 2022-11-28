48f48072447cd873fc97fed8f1cf869f33ccc104
Avishkar Vichare <avie9500@gmail.com>
2022-11-28 13:21:52 +0530

    fixed\

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -6,10 +6,12 @@ const userAuth = (req, res, next)=>{
     try{
 
         // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODM5MjY1NWNlNjcyNWEyYWY5YzI3MyIsImlhdCI6MTY2OTU2NzA3N30.U5qzJW1J5b76sA1fgLWsFPYMcXkyySAk6hZNUb0vOWE";
-        if(!token)
-        return res.status(401).json({
-            message: "No entry without auth"
-        })
+        if(!token){
+            return res.status(401).json({
+                message: "No entry without auth"
+            })
+        }
+        
         const users = jwt.verify(token,"shhhhh");
 
         // console.log(users)
